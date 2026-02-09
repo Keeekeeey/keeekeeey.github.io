635cc288ca6d0799a9f01e51a6fde3570c696b9c
keeekeeey <snowballintexas@gmail.com>
2026-02-09 12:26:56 -0600

    feat: updating links and wording

--- app/pages/project-table.tsx
+++ app/pages/project-table.tsx
@@ -8,24 +8,27 @@ export default function ProjectTable() {
                 <tbody>
                     <tr>
                         <td className=" border border-dashed p-2 font-bold">Smart Cat Feeder - website
-                            <div className="overflow-y-auto max-h-20 ">
-                                    <div className="font-normal bg-window-foreground rounded p-2"> == Used:
-                                        K3s cluster &&
-                                        Websocket &&
-                                        postgreSQL
+                            <div className="">
+                                    <div className="font-normal bg-window-foreground rounded p-2 flex flex-col">
+                                        <ul>
+                                        <li>Full-Stack Development</li>    
+                                        <li>Python</li>    
+                                        <li>PostgreSQL</li>
+                                        <li>Machine Learning</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </td>
                         <td className="border border-dashed p-2">
                             <a href="https://github.com/Keeekeeey/smart-feeder-webserver">
-                            <button className="bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
                                 <img 
                                 alt ='Github icon'
                                 className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img>
                             </button>
                             </a>
                             <a href="#">
-                            <button className="bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
                                 <img 
                                 alt ='Webpage icon'
                                 className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/128/2807/2807258.png"></img>
@@ -35,24 +38,57 @@ export default function ProjectTable() {
                     </tr>
                     <tr>
                         <td className=" border border-dashed p-2 font-bold">Smart Cat Feeder - trained model
-                            <div className="overflow-y-auto max-h-20">
-                                    <div className="font-normal bg-window-foreground rounded p-2"> == Used:
-                                        NAS &&
-                                        jupyter &&
-                                        YOLO
+                            <div className="">
+                                    <div className="font-normal bg-window-foreground rounded p-2">
+                                    <ul>
+                                        <li>Machine Learning</li>
+                                        <li>Data Pipeline Development</li>    
+                                        <li>Model Training</li>    
+                                        <li>Transfer Learning</li>
+                                    </ul>
                                     </div>
                                 </div>
                             </td>
                         <td className="border border-dashed p-2">
                             <a href="https://github.com/Keeekeeey/cat-cam">
-                            <button className="bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
                                 <img 
                                 alt ='Github icon'
                                 className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img>
                             </button>
                             </a>
                             <a href="#">
-                            <button className="bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                                <img 
+                                alt ='Webpage icon'
+                                className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/128/2807/2807258.png"></img>
+                            </button>
+                            </a>
+                        </td>
+                    </tr> 
+                    <tr>
+                        <td className=" border border-dashed p-2 font-bold">Portfolio Website
+                            <div className="">
+                                    <div className="font-normal bg-window-foreground rounded p-2">
+                                        <ul>
+                                        <li>TypeScript</li>
+                                        <li>React</li>    
+                                        <li>Next.js</li>    
+                                        <li>Componet Architecture</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </td>
+                        <td className="border border-dashed p-2">
+                            <a href="https://github.com/Keeekeeey/personal-portfolio/tree/main">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                                <img 
+                                alt ='Github icon'
+                                className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img>
+                            </button>
+                            </a>
+                            <a href="#">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
                                 <img 
                                 alt ='Webpage icon'
                                 className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/128/2807/2807258.png"></img>
@@ -61,16 +97,51 @@ export default function ProjectTable() {
                         </td>
                     </tr>
                     <tr>
-                        <td className=" border border-dashed p-2 font-bold">Geometry Node Driven Eyes
-                            <div className="overflow-y-auto max-h-20">
-                                    <div className="font-normal bg-window-foreground rounded p-2"> == Used:
-                                        Blender interal code
+                        <td className=" border border-dashed p-2 font-bold">Procederal Animation
+                            <div className="">
+                                    <div className="font-normal bg-window-foreground rounded p-2">
+                                        <ul>
+                                        <li>C#</li>
+                                        <li>Object-Oriented Programming</li>    
+                                        <li>Inverse Kinematics</li>    
+                                        <li>Unity</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </td>
+                        <td className="border border-dashed p-2">
+                            <a href="https://github.com/Keeekeeey/Procederal-Animation-in-Unity">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                                <img 
+                                alt ='Github icon'
+                                className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img>
+                            </button>
+                            </a>
+                            <a href="https://youtu.be/7agbdEiCbdQ?si=ZbVq3P39knTl91NL&t=6">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                                <img 
+                                alt ='Video icon'
+                                className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/128/0/375.png"></img>
+                            </button>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className=" border border-dashed p-2 font-bold">Vertex Influnce Tool
+                            <div className="">
+                                    <div className="font-normal bg-window-foreground rounded p-2">
+                                    <ul>
+                                        <li>Quality Assurance</li> 
+                                        <li>Maya Python API</li>
+                                        <li>Tool Development</li>    
+                                        <li>Pipeline Automation</li>    
+                                    </ul>
                                     </div>
                                 </div>
                             </td>
                         <td className="border border-dashed p-2">
-                            <a href="https://github.com/Keeekeeey/Geometry-Node-Driven-Eyes">
-                            <button className="bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                            <a href="https://github.com/Keeekeeey/Maya-Influence-Limit-Tool">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
                                 <img 
                                 alt ='Github icon'
                                 className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img>
@@ -79,32 +150,34 @@ export default function ProjectTable() {
                         </td>
                     </tr>
                     <tr>
-                        <td className=" p-2">Procedural Terrian Generation</td>
-                        <td className=" p-2">links</td>
-                    </tr>
-                    <tr>
-                        <td className=" p-2">Procedural Animation</td>
-                        <td className=" p-2">links</td>
-                    </tr>
-                    <tr>
-                        <td className=" p-2">Jaw creation tool</td>
-                        <td className=" p-2">links</td>
-                    </tr>
-                    <tr>
-                        <td className=" border border-dashed p-2 font-bold">Python Workflow Tools
-                            <div className="overflow-y-auto max-h-20">
-                                    <div className="font-normal bg-window-foreground rounded p-2"> == Used:
-                                        
+                        <td className=" border border-dashed p-2 font-bold">Python Rigging Tool Suite
+                            <div className="">
+                                    <div className="font-normal bg-window-foreground rounded p-2">
+                                        <ul>
+                                        <li>Pipeline Development</li>
+                                        <li>Cross-Platform Pipeline Support</li>    
+                                        <li>Pipeline Automation</li>  
+                                        <li>Unreal Engine Integration</li>
+                                    </ul>
                                     </div>
                                 </div>
                             </td>
                         <td className="border border-dashed p-2">
-                            <button className="bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
-                                <img className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img>
+                            <a href="https://github.com/Keeekeeey/python-rigging-tool-suite">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                                <img 
+                                alt ='Github icon'
+                                className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img>
                             </button>
-                            <button className="bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
-                                <img className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/128/2807/2807258.png"></img>
+                            </a>
+                            <a href="https://youtu.be/7agbdEiCbdQ?si=xVnk1iXWkRxMFKBq&t=79">
+                            <button className="hover:cursor-pointer bg-window-header-light rounded p-2 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-window-header-light">
+                                <img 
+                                alt ='Video icon'
+                                className="max-h-10 max-w-10" src="https://cdn-icons-png.flaticon.com/128/0/375.png"></img>
                             </button>
+                            </a>
+                            
                         </td>
                     </tr>
                 </tbody>
